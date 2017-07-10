bc260c683130bca743ac81609e86c60ee59701a4
Administrator <Administrator@JUSTMINE-PC>
2017-07-10 11:30:43 +0800

    1

--- MoneyUnitTest/MoneyUnitTest.cs
+++ MoneyUnitTest/MoneyUnitTest.cs
@@ -9,6 +9,7 @@ namespace MoneyUnitTest
         [Fact]
         public void TestAllocate2()
         {
+            //
             int[] allocation = { 3, 7 };
             Money[] result = Money.Dollars(0.05m).Allocate(allocation);
             Assert.Equal(Money.Dollars(0.02m), result[0]);
